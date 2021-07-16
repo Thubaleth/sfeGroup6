da2780447ce651ba67be18430ca099579772473c
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-07-16 15:20:39 +0200

    last commit

--- tests/test_4.3.3.py
+++ tests/test_4.3.3.py
@@ -5,7 +5,9 @@ class messageTest(unittest.TestCase):
     def test_markAsRead(self):
         sms = SMSMessage(False, "Hello", "0798653452")
         self.assertTrue(MarkASRead("read",sms))
-    
+    def test_MsgCount(self):
+        result = SMSMessage(False, "How are you?", "0631873298")
+        self.assertGreater(get_count(SMSStore), -1)
 
 if __name__ == '__main__':
     unittest.main()
